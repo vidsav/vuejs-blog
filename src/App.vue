8a82e8436a57afde5220b4260949a881e40e9bc0
Vid <savnikvid5@gmail.com>
2020-10-01 19:45:03 +0200

    addBlog component
    added addBlog vue component for adding posts

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <add-blog></add-blog>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import addBlog from './components/addBlog.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'add-blog': addBlog
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
