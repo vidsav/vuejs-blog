6e9c99a6638cb28e5dd98aba7187c9f4dab8356a
Vid <savnikvid5@gmail.com>
2020-10-04 16:09:16 +0200

    added get request and show blogs component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
-      <add-blog></add-blog>
+      <show-blogs></show-blogs>
   </div>
 </template>
 
 <script>
-import addBlog from './components/addBlog.vue'
+//import addBlog from './components/addBlog.vue'
+import showBlogs from './components/showBlogs.vue'
+
 
 export default {
   name: 'App',
   components: {
-    'add-blog': addBlog
+    //'add-blog': addBlog,
+    'show-blogs': showBlogs
   }
 }
 </script>
---
+++ src/components/showBlogs.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="show-blogs">
+      <h1>All blog articles</h1>
+      <div v-for="post in posts" v-bind:key="post.id" class="single-blog">
+        <h3>{{post.title}}</h3>
+        <article>{{post.body}}</article>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    data(){
+        return{
+            posts: []
+        }
+    },
+    methods: {
+
+    },
+    created(){
+        this.$http.get('https://jsonplaceholder.typicode.com/posts').then((data)=>{
+            //console.log(data);
+            this.posts = data.data.slice(0,10);
+        })
+    }
+}
+</script>
+
+<style>
+#show-blogs{
+    max-width: 800px;
+    margin: 0px auto;
+}
+.single-blog{
+    padding: 20px;
+    margin: 20px 0;
+    box-sizing: border-box;
+    background: #eee;
+}
+</style>
